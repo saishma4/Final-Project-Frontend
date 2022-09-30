b62c718f4f8067b02ae52f625a7a8402e2404de1
saishma4 <chanda.saishma@ivycomptech.com>
2022-09-30 12:43:29 +0530

    home page elements commit

---
+++ src/Pages/home.js
@@ -0,0 +1,42 @@
+import React from "react";
+import './home.css'
+import doll1 from '../Images/doll1.png';
+import doll2 from '../Images/doll2.png';
+import doll3 from '../Images/doll3.png';
+
+const Home = () => {
+    return(
+        <>
+        <div className="container">
+        <h1>Discover One-of-a-Kind Dolls</h1>
+            <div className="first-container">
+            </div>
+            
+            <div className="flex-container">
+                <div>
+                <img src={doll1} alt="Doll Image"/>
+                <h3>Cool Girl</h3>
+                <h3>Rs.3,400</h3>
+                <button>Add to cart</button>
+                </div>
+                <div><img src={doll2} alt="Doll Image"/>
+                <h3>Unicorn</h3>
+                <h3>Rs.5360</h3>
+                <button>Add to cart</button>
+                </div>
+                <div><img
+                src={doll3}
+                alt="Doll Image"/>
+                <h3>Paris Fashion Girl</h3>
+                <h3>Rs.4600</h3>
+                <button>Add to cart</button>
+                </div>
+            </div>
+        </div>
+        </>
+
+    ) 
+
+};
+
+export default Home;
