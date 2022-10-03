be312c8ebf44d85adba3a36dc402a07a961c2bef
saishma4 <chanda.saishma@ivycomptech.com>
2022-10-03 15:02:19 +0530

    dolls product page,product details,cart route commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import Layout from './layout';
 import Home from './Pages/home';
 import Register from './Pages/registration';
 import Login from './Pages/login';
+import DollsPage from './Pages/DollsPage';
+import DollDetailPage from './Pages/DollDetailPage';
+import CartPage from './Pages/CartPage';
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
@@ -12,6 +15,9 @@ function App() {
         <Route index element={<Home />} />
         <Route path='/login' element={<Login />} />
         <Route path='/registration' element={<Register />} />
+        <Route path="/dolls" element={<DollsPage />} />
+        <Route path="/dolls/:id" element={<DollDetailPage />} />
+        <Route path="/cart" element={<CartPage />} />
       </Route>
     </Routes>
     </BrowserRouter>
