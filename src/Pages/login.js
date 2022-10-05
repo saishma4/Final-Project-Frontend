da76d21a4d9e585a6d6837ccc9dea3c71b7150df
saishma4 <chanda.saishma@ivycomptech.com>
2022-10-05 17:43:53 +0530

    user setCookie commit

--- src/Pages/login.js
+++ src/Pages/login.js
@@ -1,71 +1,89 @@
 import React, { useState } from 'react'
 import { Outlet, Link, useNavigate } from "react-router-dom";
 import './registration.css'
+import './login.css'
 import axios from 'axios';
 
 const Login = () => {
+    const [errorMessages, setErrorMessages] = useState({});
+    const [isSubmitted, setIsSubmitted] = useState(false);
+    const [username,setUserName] = useState("");
+    const [password,setPassword] = useState("");
+
     const navigate = useNavigate()
-    const [data, setData] =useState({ 
-        username:"", 
-        password:""
-    })
-    // const [loginFormValues, setLoginFormValues] = useState({ username: "", password: "" })
 
-    const changeHandler = (e) => {
-        setData({ ...data, [e.target.name]: e.target.value })
-    }
+    const loginData = {"username": username,"password": password}
 
-    
-    // const submitHandler = (e) => {
-    //     e.preventDefault()// to prevent anything pending
-    //     if(loginFormValues.username === data.username && loginFormValues.password === data.password){
-    //         // navigate('/')
-    //        alert("login Successful")
-    //     }
-    //     else{
-    //         alert("Enter valid details")
-    //     }       
-    // }
-    const { username,password } = data;
-    const submitHandler = e => {
+    const errors = {
+        pass: "invalid username/password"
+      };
+
+    const userLogin = (e) => {
         e.preventDefault();
-        addDataToServer(data)
+        authenticateUser(loginData)
     }
-
-    const addDataToServer = (cred) => {
-        console.log(cred);
-        axios.post("http://localhost:8080/api/auth/login", cred).then(
+    const setCookie= (cname, cvalue, exdays) => {
+        const d = new Date();
+        d.setTime(d.getTime() + (exdays*24*60*60*1000));
+        let expires = "expires="+ d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+      }
+      const authenticateUser = (data) => {
+        console.log(data);
+        axios.post("http://localhost:8080/api/auth/login", data).then(
             (response) => {
                 
                 console.log(response);
-                // alert("user signed in Successfully");
-                if (response.status==200) {
-                  console.log("navigating");
-                  navigate('/');
-              }
+                if (response.status===200) {
+                    console.log("navigating");
+                    setCookie("accessToken",response.data.accessToken,1);
+                    setIsSubmitted(true);
+                    window.location.href = '/';
+                    
+                }
             }, (error) => {
-                console.log(error);
-                alert("Operation failed");
+              setErrorMessages({ name: "pass", message: errors.pass });
             }
         );
     }
 
-
-    return (
+    const onInputChangeUsername = event => {
+        setUserName(event.target.value)
+    }
+    
+    const onInputChangePassword = event => {
+        setPassword(event.target.value)
+    }
+    
+    const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+    );
+    const renderForm=(
         <div className='register-container'>
             <form >
                 <h1>Login</h1>
-                <label>Username</label>
+                <label htmlFor="loginUser">User Name</label>
                 <br />
-                <input type="text" name="username" placeholder='Enter username' value={username} onChange={changeHandler} />
+                <input type="text" name="uname" placeholder='Enter username' value={username} onChange={e => onInputChangeUsername(e)} required />
+                {renderErrorMessage("uname")}
                 <br />
-                <label>Password</label>
+                <label htmlFor="loginPassword">Password</label>
                 <br />
-                <input type="password" name="password" placeholder='Enter Password' value={password} onChange={changeHandler} />
+                <input type="password" name="pass" placeholder='Enter Password' value={password} onChange={e => onInputChangePassword(e)} required/>
+                {renderErrorMessage("pass")}
                 <br />
-                <button className='signup-button' onClick={submitHandler} id="submit">Login</button>
+                <button type="submit" className='signup-button'value="Login" onClick={userLogin} id="submit">Login</button>
+                <div className='forgotpassword'>Forgot Password?</div>
             </form>
         </div>
+    );
+
+    return (
+        <div>
+            {isSubmitted ? navigate("/") : renderForm}
+        </div>
+        
     );
 }
 
