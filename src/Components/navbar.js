63d50f0bacffd83b176b2d5a581da816b92f05c0
saishma4 <chanda.saishma@ivycomptech.com>
2022-10-05 17:44:36 +0530

    user logout commit

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -1,34 +1,77 @@
 import React from "react";
 import './style.css';
-import {Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import WebFont from 'webfontloader';
 import { useEffect } from "react";
 
-const Navbar = () =>{
+const Navbar = () => {
+    const getCookie = (cname) => {
+        let name = cname + "=";
+        let decodedCookie = decodeURIComponent(document.cookie);
+        let ca = decodedCookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) === ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) === 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
+
+    const signoutHandler = () => {
+        console.log("logout")
+        console.log(window.location);
+        document.cookie = "accessToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        window.location.href = '/';
+    }
+
     useEffect(() => {
         WebFont.load({
-          google: {
-            families: ['Droid Sans', 'Alkalami']
-          }
+            google: {
+                families: ['Droid Sans', 'Alkalami']
+            }
         });
-       }, []);
-    return(
-    <nav className="nav">
-        <div className="logo"><img 
-        src="https://mystickermania.com/cdn/stickers/noob-pack/cute-smiling-unicorn-512x512.png"
-        alt="absolute url"/>
-        <Link to="/" className="site-title" style={{fontFamily: 'Alkalami'}}>dollza</Link>
+    }, []);
+    return (
+        <div>
+            {getCookie("accessToken") &&
+                <nav className="nav">
+                    <div className="logo"><img
+                        src="https://mystickermania.com/cdn/stickers/noob-pack/cute-smiling-unicorn-512x512.png"
+                        alt="absolute url" />
+                        <Link to="/" className="site-title" style={{ fontFamily: 'Alkalami' }}>dollza</Link>
+                    </div>
+                    <ul>
+                        <li>
+                            <a href="/dolls">Dolls</a>
+                            <a href="#">Orders</a>
+                            <a href="/cart">Cart</a>
+                            <a href="/" onClick={signoutHandler}>Logout</a>
+                        </li>
+                    </ul>
+                </nav>
+            }
+            {!(getCookie("accessToken")) &&
+                <nav className="nav">
+                    <div className="logo"><img
+                        src="https://mystickermania.com/cdn/stickers/noob-pack/cute-smiling-unicorn-512x512.png"
+                        alt="absolute url" />
+                        <Link to="/" className="site-title" style={{ fontFamily: 'Alkalami' }}>dollza</Link>
+                    </div>
+                    <ul>
+                        <li>
+                            <a href="/dolls">Dolls</a>
+                            <Link to="/registration">Register </Link>
+                            <Link to="/login">Login</Link>
+                        </li>
+                    </ul>
+                </nav>
+            }
+
         </div>
-        <ul>
-            <li>
-                <a href="/dolls">Dolls</a>
-                <a href="#">Orders</a>
-                <Link to="/registration">Register </Link>
-                <Link to="/login">Login</Link>
-                <a href="/cart">Cart</a>
-            </li>
-        </ul>
-    </nav>
     );
 
 }
