a57c2a290c55a7af495e8ac05cc29f790b26c285
saishma4 <chanda.saishma@ivycomptech.com>
2022-10-07 11:31:10 +0530

    about website  commit

---
+++ src/Pages/about.js
@@ -0,0 +1,26 @@
+import { margin } from "@mui/system";
+import React from "react";
+import './home.css'
+
+
+const About = () => {
+    
+    return(
+        <div
+      style={{
+        textAlign:'center',
+        marginTop:'10%',
+        height: '60vh',
+        marginLeft:'20%',
+        marginRight:'20%'
+      }}
+    >
+        <h2>
+        For the good
+        <p>Dollza is an online marketplace</p>
+        where people come to buy and collect unique dolls.
+        We’re also a community pushing for positive change for small businesses, people, and the planet. 
+        </h2>
+    </div>
+    )}
+export default About;
